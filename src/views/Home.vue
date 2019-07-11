debe43f1557855fb63091745f8fe0616585e052e
forijk <yuyinjian@mafengwo.com>
2019-07-11 22:30:49 +0800

    onclick

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  mounted() {
+    const arr = document.getElementsByTagName('li');
+    for(let i = 0; i<arr.length; i++) {
+      arr[i].onclick = function() {
+        console.log(i);
+      }
+    }
   }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
+<style scoped>
+li {
+  width: 100%;
+  color: grey;
+  border: 1px solid blue;
+}
+</style>
+
